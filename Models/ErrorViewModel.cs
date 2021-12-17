45b1d4e0e338e9006d6880b2fe49ae6c70fcf8fe
min <min@DESKTOP-T2NAVI1>
2021-12-17 15:06:30 +0700

    Fisrt_commit_LNG136

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LeNhatGiang136.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
